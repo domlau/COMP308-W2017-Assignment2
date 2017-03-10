9a10ac0a3f199af4bb50cb5b8f3539b79aa832cd
domlau <dominic.k.y.lau@gmail.com>
2017-03-10 15:11:41 -0500

    add routes for login and register

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,15 +1,30 @@
 let express = require('express');
 let router = express.Router();
 let mongoose = require('mongoose');
+let passport = require('passport');
 
-//create user object that represents the document in assignment collection
-let user = require('../models/users');
+//define the user models
+let UserModel = require('../models/users');
+let User = UserModel.User;
+
+//define the contact models
+let contact = require('../models/contacts');
+
+//function to check for user authentication
+function requireAuth(req,res,next) {
+  //check for user logged
+  if(!req.isAuthenticated()){
+    return res.redirect('/login');
+  }
+  next();
+}
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
   res.render('content/index', {
     title: 'Dominic Portfolio Site',
-    users:''
+    contacts:'',
+    displayName: req.user ? req.user.displayName: ''
   });
 });
 
@@ -17,7 +32,8 @@ router.get('/', (req, res, next) => {
 router.get('/about', (req, res, next) => {
   res.render('content/about', { 
     title: 'About Me',
-    users:''
+    contacts:'',
+    displayName: req.user ? req.user.displayName: ''
   });
 });
 
@@ -25,7 +41,8 @@ router.get('/about', (req, res, next) => {
 router.get('/projects', (req, res, next) => {
   res.render('content/projects', { 
     title: 'My Projects',
-    users:'' 
+    contacts:'',
+    displayName: req.user ? req.user.displayName: '' 
   });
 });
 
@@ -33,7 +50,8 @@ router.get('/projects', (req, res, next) => {
 router.get('/services', (req, res, next) => {
   res.render('content/services', { 
     title: 'My Services',
-    users:'' 
+    contacts:'',
+    displayName: req.user ? req.user.displayName: '' 
   });
 });
 
@@ -41,7 +59,86 @@ router.get('/services', (req, res, next) => {
 router.get('/contact', (req, res, next) => {
   res.render('content/contact', { 
     title: 'Contact Me',
-    users:''
+    contacts:'',
+    displayName: req.user ? req.user.displayName: ''
   });
 });
+
+/*GET /login - rendering login view */
+router.get('/login', (req,res,next) => {
+  //check if user is logged in
+  if(!req.user){
+    //render login page
+    res.render('auth/login', {
+      title: "Login",
+      contacts:'',
+      messages: req.flash('loginMessage'),
+      displayName: req.user ? req.user.displayName: ''
+    });
+    return;
+  } else {
+    return res.redirect('/contacts');
+  }
+});
+
+/*POST /login - process the login authentication */
+router.post('/login',passport.authenticate('local',{
+  successRedirect:'/contacts',
+  failureRedirect:'/login',
+  failureFlash:'Incorrect Login'
+}));
+
+/*GET /register - render registration view */
+router.get('/register', (req,res,next) => {
+  //check if user is registered/logged in
+  if(!req.user) {
+    //rengder registration page
+    res.render('auth/register', {
+      title: 'Register',
+      contacts:'',
+      messages: req.flash('registerMessage'),
+      displayName: req.user ? req.user.displayName: ''
+    });
+    return;
+  } else {
+    return res.redirect('/contacts');
+  }
+});
+
+/*POST /register - process the registration */
+router.post('/register', (req,res,next) => {
+  User.register(
+    new User({
+      username: req.body.username,
+      email: req.body.email,
+      displayName: req.body.displayName
+    }),
+    req.body.password,
+    (err) => {
+      if (err) {
+        console.log('Error inserting new user');
+        if(err.name == 'UserExistsError') {
+          req.flash('registerMessage','Registration Error: User already exists');
+        }
+        return res.render('/auth/register',{
+          title: 'Register',
+          contacts: '',
+          messages: req.flash('registerMessage'),
+          displayName: req.user ? req.user.displayName : ''
+        });
+      }
+      //when successful registered
+      return passport.authenticate('local')(req,res,() =>{
+        res.redirect('/contacts');
+      });
+    });
+});
+
+/*GET Logout */
+router.get('/logout', (req,res,next) => {
+  req.logout();
+  //redirect back to homepage
+  res.redirect('/');
+});
+
 module.exports = router;
