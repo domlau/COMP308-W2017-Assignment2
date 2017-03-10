737a92ced86d54e2228e4585d52d9e4d0d7f0ca9
domlau <dominic.k.y.lau@gmail.com>
2017-03-10 14:44:01 -0500

    set up user model

--- server/models/users.js
+++ server/models/users.js
@@ -1,15 +1,46 @@
 //import npm mongoose package
 let mongoose = require('mongoose');
+let Schema = mongoose.Schema;
+let passportLocalMongoose = require('passport-local-mongoose');
 
 //create model class
-let userSchema = mongoose.Schema({
-    userName: String,
-    password: String,
-    email: String
+let userSchema = new Schema({
+    userName: {
+        type: String,
+        default:'',
+        trim:true,
+        required:'username is required'
+    },
+    password: {
+        type: String,
+        default: '',
+        trim: true,
+        required: 'password is required'
+    },
+    email: {
+        type: String,
+        default:'',
+        trim:true,
+        required: 'email is required'
+    },
+    displayName:{
+        type:String,
+        default:'',
+        trim:true,
+        required:'Display name is required'
+    },
+    created: {
+        type:Date,
+        default: Date.now
+    } 
 },
 {
     collection:"user"
 });
+
+let options = ({missingPasswordError: "wrong password"});
+
+userSchema.plugin(passportLocalMongoose,options);
 
 //db = name of this file that represent the document structure
 module.exports = mongoose.model('users',userSchema);
