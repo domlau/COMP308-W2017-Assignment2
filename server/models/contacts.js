61fe033a0b68c22501a2d43272085e1329d26c31
domlau <dominic.k.y.lau@gmail.com>
2017-03-10 14:48:01 -0500

    create model class for contacts

---
+++ server/models/contacts.js
@@ -0,0 +1,13 @@
+let mongoose = require('mongoose');
+let Schema = mongoose.Schema;
+//create model class for contacts
+let contactSchema = new Schema({
+    contactName: String,
+    contactNumber: String,
+    emailAddress: String
+},
+{
+    collection:'contacts'
+});
+
+module.exports = mongoose('contacts',contactSchema);
